338cc5b6fce4932128154b9319c66b2e59915dd8
wesbubeck <wesbubeck@gmail.com>
2023-08-21 14:04:05 -0400

    text spacing

--- src/pages/landingPage.tsx
+++ src/pages/landingPage.tsx
@@ -6,18 +6,22 @@ const LandingPage = () => {
         <Paper shadow="md" p="xl">
             <Container size="md">
                 <Title order={1}>Good luck!</Title>
+                &nbsp;
                 <Text>
                     Please refer to README.md or reach out with any questions
                 </Text>
+                &nbsp;
                 <Text>
                     It is optional to use Mantine Components&nbsp;&nbsp;
                     <Anchor href="https://mantine.dev/" target="_blank">
                         Mantine docs
                     </Anchor>
                 </Text>
+                &nbsp;
                 <Text>
                     Create a table of reps that includes the following columns: First Name, Last Name, Email, Team, Total Revenue
                 </Text>
+                &nbsp;
                 <Text>
                     Add the following filters: Team Name, Opp Customer
                 </Text>
